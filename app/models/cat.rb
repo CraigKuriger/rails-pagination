f7d94995b921c9e6c83d5c1463ee37fa2dfc0f89
Craig Kuriger <craig.kuriger@crssoftware.co.nz>
2020-02-23 10:48:21 +1300

    create method to get offset records

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,5 @@
 class Cat < ApplicationRecord
+  def self.get_offset_selection(max_records, page_number)
+    offset(max_records * page_number).limit(max_records)
+  end
 end
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'get_offset_selection' do
+    before(:example) do
+      100.times do
+        FactoryBot.create(:cat)
+      end
+    end
+    it 'returns the max number of records' do
+      result = Cat.get_offset_selection(10, 5)
+      result.count.should be 10
+    end
+
+    it 'return records beginning after the offset ' do
+      result = Cat.get_offset_selection(10, 5)
+      result.first.id.should be 51
+      result.last.id.should be 60
+    end
+  end
 end
